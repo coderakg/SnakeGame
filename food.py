b9cb26dcae4347c2308d6f48f412d420f4bbea50
Akshat Girdhar <84849106+coderakg@users.noreply.github.com>
2022-11-21 19:32:52 +0530

    Update food.py

--- food.py
+++ food.py
@@ -1,21 +1,17 @@
-# the food file will contain a class food that will create small dot that will be the food object
-# the food will re appear each time the snake eats it in a new location
-
-colors = ["red","white","light blue","pink"]
+colors = ["red","green","blue","pink"]
 
 from turtle import Turtle
 import random
-# making a good class that will inherit from the turtle class
+
 class Food(Turtle):
     def __init__(self) :
-        super().__init__() # inheriting the turtle class with the super 
+        super().__init__()  
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5,stretch_wid=0.5) 
         self.color(random.choice(colors))
-        self.speed("fastest") # to avoid watching the animation
+        self.speed("fastest") 
         self.refresh()
-         # the position of the food will be decided randomly
 
         
     def refresh(self):
