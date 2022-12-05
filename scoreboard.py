2cc83ea69a0be6b8d7c9c04d3a45719ed78fe2f2
Akshat Girdhar <84849106+coderakg@users.noreply.github.com>
2022-12-05 20:32:48 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,8 +8,6 @@ class Scoreboard(Turtle):
     def __init__(self) :
         super().__init__ ()
         self.score = 0
-        with open("snake_game\data.txt") as file:
-            self.high_score = int(file.read())
         self.hideturtle()
         self.penup()
         self.setposition(0,260)
@@ -17,18 +15,16 @@ class Scoreboard(Turtle):
         self.update_score()
         
     def update_score(self):
-        self.clear()
-        self.write(f"Score : {self.score} || High Score : {self.high_score}",align=ALIGNMENT,font = FONT)
+        self.write(f"Score : {self.score}",align=ALIGNMENT,font = FONT)
         
-    def reset_score(self):
-        if self.score > self.high_score :
-            self.high_score = self.score
-            with open("snake_game\data.txt",mode="w") as data:
-                data.write(f"{self.high_score}")
-            
-        self.score = 0 
-        self.update_score() 
+    def game_over(self):
+        self.color("red")
+        self.setposition(0,0)
+        self.write(f"GAME OVER",align=ALIGNMENT,font = ("Courier",50,"normal"))
+        self.setposition(0,-60)
+        self.write(f"Final Score: {self.score}",align=ALIGNMENT,font = ("Indie Flower",50,"normal"))
         
     def inc_score(self):
         self.score += 1
+        self.clear()
         self.update_score()
